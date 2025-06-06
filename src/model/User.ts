24bd25e01541fcdf86df4499411fb6915b42bdbe
kamal20247 <147083931+kamalgurjar7@users.noreply.github.com>
2025-06-06 15:31:26 +0530

    schema created

---
+++ src/model/User.ts
@@ -0,0 +1,58 @@
+import mongoose , {Schema , Document} from "mongoose";
+
+ export interface Message extends Document{
+    content:string;
+    createdAt:Date
+}
+
+const MessageSchema : Schema <Message>= new Schema({
+    content: {
+        type:String,
+        required:true 
+    },
+    createdAt : {
+        type:Date,
+        required:true,
+        default:Date.now
+    }
+})
+
+
+
+export interface User extends Document {
+    username : string ;
+    email: string;
+    password:string;
+    verifyCode:string;
+    verifyCodeExpiry:Date;
+    isAcceptingMessage:boolean;
+    message: Message[]
+}
+
+const UserSchema : Schema <User>= new Schema({
+   username :{
+    type:String,
+    required: [true,"Username is required"],
+    unique:true
+   },
+    email :{
+    type:String,
+    required: [true,"email is required"],
+    unique:true,
+    match : [/.+\@.+..+/,'please use a valid email address']
+   },
+    password :{
+    type:String,
+    required: [true,"password is required"]
+   },
+    verifyCode :{
+    type:String,
+    required: [true,"verifycode is required"]
+   },
+    verifyCodeExpiry:{
+    type:Date,
+    required: [true,"verify code expiry is required"]
+   },
+    isAcceptingMessage:boolean,
+    message: Message[]
+})
